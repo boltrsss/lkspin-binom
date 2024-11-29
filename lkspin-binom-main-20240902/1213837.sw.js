0bfe1262d58e1d80812d1905d9b5c60b11efab8e
boltrsss <163812338+boltrsss@users.noreply.github.com>
2024-11-29 17:28:14 +0800

    Delete lkspin-binom-main-20240902 directory

--- lkspin-binom-main-20240902/1213837.sw.js
+++
@@ -1,5 +0,0 @@
-self.opts = {
-    zoneID: 1213837,
-    swDomain: "push-sdk.com",
-}
-importScripts("https://push-sdk.com/f/sw.js")
--- lkspin-binom-main-20240902/1238026.sw.js
+++
@@ -1,5 +0,0 @@
-self.opts = {
-    zoneID: 1238026,
-    swDomain: "sdk4push.com",
-}
-importScripts("https://sdk4push.com/f/sw.js")
